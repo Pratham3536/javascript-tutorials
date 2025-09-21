e3d8ba3014d7a5de66899ef5fc90df205ffedad3
pratham3536 <wandhareprathamesh4@gmail.com>
2025-09-21 12:53:10 +0530

    JS Files

---
+++ first.js
@@ -0,0 +1,37 @@
+// java script is dynamic language 
+// it is case sensative 
+// camel notation used in the variable name 
+
+
+ let fullName= " tony start ";// using let we can not the re declare it only update it
+{
+ let fullName= " tony";
+ console.log(fullName);
+}
+ console.log(fullName);
+
+  var  age = 23; // for the var keyword we can  redeclare as well as update the variable 
+
+  var  age = 24; 
+
+   price= 343.333; 
+   x = null; // no value 
+
+   y= undefined;  
+   isFollow= true ; // boolean datatype 
+       // object are declared by constant generally  
+
+console.log(isFollow);
+
+   
+// object are declared by constant generally  
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
+};
+
+
+// console.log(product);
+// console.log(product["offer"]);
---
+++ second.js
@@ -0,0 +1,15 @@
+const accountId= 23232; 
+let accoutEmail= " wandhaerprathaemsh@gmail.com";
+var accountPassword = " 12211";
+
+accountCity= "Jaipur"; 
+
+// accountId= 2 ; //not allowed as it is constant 
+
+accoutEmail= "hac@h.com"; 
+accountPassword=" 242323"; 
+accountCity= " mumbai"; 
+
+console.log(accountId); 
+
+ console.table([accountId, accountPassword, accoutEmail]);
